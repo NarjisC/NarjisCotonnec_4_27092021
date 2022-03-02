40e865acae2e3b57bc2f1feadf2bc2bc2f69d002
NarjisC <80031207+NarjisC@users.noreply.github.com>
2022-03-02 19:24:15 +0100

    input messages

--- modal.js
+++ modal.js
@@ -1,3 +1,5 @@
+// responsive nav
+
 function editNav() {
   document.getElementById("myTopNav").classList.toggle("show");
   console.log(click);
@@ -16,17 +18,48 @@ window.onclick = function(event) {
   }
 }
 
-// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modal = document.getElementById('modal');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//Input
+// First Name
+function firstName() {
+  const firstNameInput = document.getElementById('firstname').value;
+  const firstErrorMsg = document.querySelector(".firstErrorMsg");
+  const isFirstNameValid = firstNameInput.trim().length >= 2;
+
+  if (isFirstNameValid) {
+    firstErrorMsg.classList.add('hidden');
+  } else {
+    firstErrorMsg.classList.remove('hidden');
+  }
+  return isFirstNameValid;
+}
+
+//Last Name
+function lastName() {
+  const lastNameInput = document.getElementById('lastname').value;
+  const $lastErrorMsg = document.querySelector('.lastErrorMsg');
+  const isLastNameValid = lastNameInput.trim().length >= 2;
+
+  if (isLastNameValid) {
+    $lastErrorMsg.classList.add('hidden');
+  } else {
+    $lastErrorMsg.classList.remove('hidden');
+  }
+  return isLastNameValid;
+}
+
 
 
